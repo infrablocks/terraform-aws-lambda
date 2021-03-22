4fd5b711462ba5175024c101f274657c490a4d5d
Toby Clemson <tobyclemson@gmail.com>
2021-03-22 12:03:33 +0000

    Add qualified arn output

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,9 @@ output "lambda_invoke_arn" {
 output "lambda_arn" {
   value = aws_lambda_function.lambda.arn
 }
+output "lambda_qualified_arn" {
+  value = aws_lambda_function.lambda.qualified_arn
+}
 output "lambda_function_name" {
   value = aws_lambda_function.lambda.function_name
 }
